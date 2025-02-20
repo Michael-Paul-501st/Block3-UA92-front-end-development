47cff20df3729fd669278daf83f877f580d7b061
Michael Paul <michael0110p@gmail.com>
2025-02-20 09:29:43 +0000

    Delete Project folder - artificium directory

--- Project folder - artificium/Comment_Script.js
+++
@@ -1,9 +0,0 @@
-function Comment_script(){
-    let comment = document.getElementById("comment").value;//get the value from comment
-    if (comment != ""){//check if the commet has a value in it
-        alert("comment Posted")
-    }
-    else {
-        alert("invalid Comment")
-    }
-}
